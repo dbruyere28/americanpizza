ebf3e03671acda5a6855e000fc3de6ac75a128cd
dbruyere28 <145119766+dbruyere28@users.noreply.github.com>
2026-03-02 18:30:11 +0100

    Create Index.tsx

---
+++ src/components/Index.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import MenuSection from "@/components/MenuSection";
+import ContactSection from "@/components/ContactSection";
+
+const Index = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <MenuSection />
+      <ContactSection />
+    </div>
+  );
+};
+
+export default Index;
